33150bb4175f19700814e341c37f6718a3decfc0
sewwandi edirisooriya <sewwandiedirisooriya318@gmail.com>
2023-03-24 10:16:27 -0700

    java

---
+++ src/Company.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.Callable;
+
+public class Company {
+    String brandName;
+    Company (){
+        brandName = "Apple";
+
+    }
+
+    public static void main(String[] args) {
+        Company c = new Company();
+        System.out.println(c.brandName);
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    Main(){
+
+        String name ="programming language is ";
+        System.out.println( name + "java");
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+}
